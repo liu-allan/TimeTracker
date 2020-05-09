e7d5f8397f7adc22bd134a496aa929cad39a8327
Allan Liu <44624435+liu-allan@users.noreply.github.com>
2020-05-09 18:30:13 -0500

    added other inputs, q -> 5 most used, t -> all, g -> creates a pie chart

--- timetracker.py
+++ timetracker.py
@@ -1,4 +1,5 @@
 import time
+import matplotlib.pyplot as plt
 from threading import Thread
 from win32 import win32gui, win32api, win32console, win32process
 import psutil
@@ -33,9 +34,9 @@ def timer():
         if(currentApplicationName != previousApplicationName):
             currentApplicationElapsed = time.time() - currentApplicationStart
             # store the value into the dictionary
-            if(applications.get(previousApplicationName) == None and previousApplicationName != ""):
+            if(applications.get(previousApplicationName) == None and previousApplicationName != "" and previousApplicationName != "python.exe"):
                 applications[previousApplicationName] = currentApplicationElapsed
-            elif (previousApplicationName != ""):
+            elif (previousApplicationName != "" and previousApplicationName != "python.exe"):
                 applications[previousApplicationName] += currentApplicationElapsed
 
             # record the current application start time
@@ -51,14 +52,53 @@ applications = {}
 backgroundThread = Thread(target=timer, args=( ))
 backgroundThread.start()
 start = time.time()
+
+# main loop
 while True:
+    # poll user input
     keypressed = input()
+
+    # if user pressed q, print the statistics for the 5 most used apps
     if(keypressed == 'q'):
         globalElapsed = time.time() - start
+
+        # display current statistics
         print("Total Time Elapsed: " + time.strftime("%H:%M:%S", time.gmtime(globalElapsed)) + "\n")
-        for x in applications:
-            print("Application Name: " + x + " Time: " + time.strftime("%H:%M:%S", time.gmtime(applications[x])))
-            percentage = 100 * applications[x] / globalElapsed
+
+        applications_sorted = {k: v for k, v in sorted(applications.items(), key=lambda x: x[1], reverse=True)}
+
+        counter = 0
+        for x in applications_sorted:
+            print("Application Name: " + x )
+            print("Time: " + time.strftime("%H:%M:%S", time.gmtime(applications_sorted[x])))
+            percentage = 100 * applications_sorted[x] / globalElapsed
+            print("Percent of Total Time: " + "%.2f" % percentage + "%\n")
+            counter += 1
+            if (counter > 4):
+                break
+
+    # if user pressed t, print all the statistics
+    elif(keypressed == 't'):
+        globalElapsed = time.time() - start
+
+        # display current statistics
+        print("Total Time Elapsed: " + time.strftime("%H:%M:%S", time.gmtime(globalElapsed)) + "\n")
+
+        applications_sorted = {k: v for k, v in sorted(applications.items(), key=lambda x: x[1], reverse=True)}
+
+        for x in applications_sorted:
+            print("Application Name: " + x)
+            print("Time: " + time.strftime("%H:%M:%S", time.gmtime(applications_sorted[x])))
+            percentage = 100 * applications_sorted[x] / globalElapsed
             print("Percent of Total Time: " + "%.2f" % percentage + "%\n")
 
+    elif(keypressed == 'g'):
+        # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+        labels = applications.keys()
+        sizes = applications.values()
 
+        fig1, ax1 = plt.subplots()
+        ax1.pie(sizes, explode= None, labels= labels, autopct='%1.1f%%', shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+        plt.show()
